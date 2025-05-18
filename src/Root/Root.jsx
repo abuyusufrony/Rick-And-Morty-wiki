6c88e6335b474621f4fc9729af4caf4e4820336b
abuyusufrony <abuyusufrony@gmail.com>
2025-05-19 01:29:46 +0600

    styling-in-wrong-way

--- src/Root/Root.jsx
+++ src/Root/Root.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Outlet } from 'react-router';
 import Banner from '../Banner/Banner';
+import Characters from '../Components/Characters/Characters';
 
 const Root = () => {
     return (
-        <div>
+        <div className='w-11/12 mx-auto'>
             <Banner></Banner>
+            <Characters></Characters>
 
         </div>
     );
